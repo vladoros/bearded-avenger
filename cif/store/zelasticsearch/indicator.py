fe79148c46035e55d4ff7808d557a4c69c737c0d
Vlad Oros <voros@opswat.com>
2018-11-19 17:49:14 +0200

    fix #418 catch bulk create error

--- cif/store/zelasticsearch/indicator.py
+++ cif/store/zelasticsearch/indicator.py
@@ -158,7 +158,11 @@ class IndicatorManager(IndicatorManagerPlugin):
             ii = self.create(token, i, bulk=True)
             actions.append(ii)
 
-        helpers.bulk(self.handle, actions, index=self._current_index())
+        try:
+            helpers.bulk(self.handle, actions, index=self._current_index())
+        except Exception as e:
+            logger.error(e)
+            actions = []
 
         if flush:
             self.flush()
